a9bed7433c7e58a116d3028efa0e2929adbe5819
Kyle Lin <kylejlin.us@gmail.com>
2017-02-21 16:07:01 -0800

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,20 @@
+var fileUploader = document.getElementById('file-upload');
+var originalImg = document.getElementById('original');
+var invertedImg = document.getElementById('inverted');
+
+fileUploader.addEventListener('change', function() {
+  var file = fileInput.files[0],
+            reader = new FileReader();
+        
+        if (file instanceof File && /\.(jpe?g|png|gif)$/i.test(file.name)) {
+            reader.addEventListener('load', function() {
+                originalImg.src = this.result;
+                invertedImg.src = invertColors(originalImg).src;
+            });
+            
+            reader.readAsDataURL(file);
+        } else {
+            error('As of now, this tool only supports jpg, jpeg, png, and gif files.');
+        }
+    });
+});
