0a89f071b73a8166831f705e02b6dfd16464fc91
Kyle Lin <kylejlin.us@gmail.com>
2017-02-21 21:46:29 -0800

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,15 +1,23 @@
 var fileUploader = document.getElementById('file-upload');
 var originalImg = document.getElementById('original');
 var invertedImg = document.getElementById('inverted');
+var downloadedFileNameInput = document.getElementById('download-name');
+var downloader = document.getElementById('download');
 
 fileUploader.addEventListener('change', function() {
   var file = fileUploader.files[0];
+  var fileName = file.name;
   var reader = new FileReader();
         
   if (file instanceof File && /\.(jpe?g|png|gif)$/i.test(file.name)) {
     reader.addEventListener('load', function() {
       originalImg.src = this.result;
-      invertedImg.src = invertColors(originalImg).src;
+      
+      var invertedImgDataURL = invertColors(originalImg).src;
+      invertedImg.src = invertedImgDataURL;
+      
+      downloader.href = invertedImgDataURL;
+      downloader.download = downloadedFileNameInput.input ? downloadedFileNameInput : '(inverted color) ' + fileName;
     });
             
     reader.readAsDataURL(file);
